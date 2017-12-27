eaf7df2843c2f91e01ccdfbcdc67cc64645af24a
Massinissa Lounis <lounismassinissa@gmail.com>
2017-12-27 10:44:25 +0100

    saccessfully loaded static resources

---
+++ souk/src/main/java/net/souk/controller/PageController.java
@@ -0,0 +1,43 @@
+package net.souk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value={"/","home","index"})
+	public ModelAndView index(){
+		ModelAndView mv  = new ModelAndView("page");
+		mv.addObject("greeting","Wellcome to spring MVC");
+		return mv;
+	}
+
+	
+
+}
+
+
+
+
+//@RequestMapping(value={"/test"})
+//public ModelAndView test(@RequestParam(value = "greeting" ,required = false)String greeting){
+//	
+//	if(greeting  == null ){
+//		greeting  = "default value";
+//	}
+//	ModelAndView mv  = new ModelAndView("page");
+//	mv.addObject("greeting",greeting);
+//	return mv;
+//}
+//
+//@RequestMapping(value={"/test/{greeting}"})
+//public ModelAndView test(@PathVariable("greeting")String greeting){
+//	if(greeting  == null ){
+//		greeting  = "default value";
+//	}
+//	ModelAndView mv  = new ModelAndView("page");
+//	mv.addObject("greeting",greeting);
+//	return mv;
+//}
